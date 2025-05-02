9da9722413e46b3fdb9a72d5235bb22bbaac6975
Kamal Sunar <kkskgamer@gmail.com>
2025-05-02 13:13:23 +0545

    added mouse click listner

--- include/mouse.h
+++ include/mouse.h
@@ -2,10 +2,13 @@
 #define MOUSE_H
 
 #include <SDL3/SDL.h>
+#include <stdbool.h>
 
 extern SDL_FPoint mousePosition;
+extern bool cursor_on_menu;
+extern bool clicked_menu;
 
 void check_hover(void);
-
+void check_click(SDL_Event *event);
 
 #endif
--- src/mouse.c
+++ src/mouse.c
@@ -3,23 +3,36 @@
 #include <SDL3/SDL.h>
 
 SDL_FPoint mousePosition;
+bool cursor_on_menu = false;
+bool clicked_menu = false;
 
-void check_hover() {
-    SDL_GetMouseState(&mousePosition.x, &mousePosition.y);
-
-    if (SDL_PointInRectFloat(&mousePosition, &MENU_POSITION)) {
+void toggle_cursor(bool state) {
+    if (state) {
         SDL_Cursor *cursor_pointer = SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_POINTER);
         if (!cursor_pointer) {
             SDL_Log("Failed to create cursor: %s", SDL_GetError());
-            return;
         }
         SDL_SetCursor(cursor_pointer);
     } else {
         SDL_Cursor *cursor_normal = SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_DEFAULT);
         if (!cursor_normal) {
             SDL_Log("Failed to create cursor: %s", SDL_GetError());
-            return;
         }
         SDL_SetCursor(cursor_normal);
+    }
+}
+
+void check_hover() {
+    SDL_GetMouseState(&mousePosition.x, &mousePosition.y);
+    cursor_on_menu = SDL_PointInRectFloat(&mousePosition, &MENU);
+
+    toggle_cursor(cursor_on_menu);
+}
+
+void check_click(SDL_Event *event) {
+    if (event->type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
+        if (event->button.button == SDL_BUTTON_LEFT){
+            clicked_menu = !clicked_menu;
+        }
     }
 }
